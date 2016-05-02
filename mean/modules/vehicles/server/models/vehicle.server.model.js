ec7587d81a3d2ac31ede804e145aa194ef2e0171
caavere <cody.averett@walmart.com>
2016-05-01 21:51:58 -0500

    Added Vehicle mongo schema

--- mean/modules/vehicles/server/models/vehicle.server.model.js
+++ mean/modules/vehicles/server/models/vehicle.server.model.js
@@ -10,20 +10,55 @@ var mongoose = require('mongoose'),
  * Vehicle Schema
  */
 var VehicleSchema = new Schema({
-  name: {
-    type: String,
-    default: '',
-    required: 'Please fill Vehicle name',
-    trim: true
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Vehicle registration name',
+		trim: true
+	},
+  type: {
+      type: String,
+      default: '',
+      required: 'Please fill the Vehicle Type',
+      trim: true
   },
-  created: {
-    type: Date,
-    default: Date.now
+  year: {
+      type: Number,
+      required: 'Please fill in the Vehicle Year',
+      trim: true 
   },
-  user: {
-    type: Schema.ObjectId,
-    ref: 'User'
-  }
+  make: {
+      type: String,
+      required: 'Please fill in the Vehicle Make',
+      trim: true  
+  },
+  model: {
+      type: String,
+      required: 'Please fill in the Vehicle Model',
+      trim: true
+  },
+  mods: {
+      type: String,
+      required: 'Please fill in the Vehicle Mods'  
+  },
+  class: {
+      type: String,
+      required: 'Please choose a Vehicle Class'  
+  },
+  hasPayed: {
+      type: Boolean
+  },
+  registrationNumber: {
+    type: Number,
+  },
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
 });
 
 mongoose.model('Vehicle', VehicleSchema);
