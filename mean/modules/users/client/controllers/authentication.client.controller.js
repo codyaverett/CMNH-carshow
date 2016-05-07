3a8d15108ac5e76eb55cb8df98616f9278b80180
Cody Averett <caavere@L-HOA5A2YFFT-M.local>
2016-05-06 20:06:21 -0500

    Matching gui to schema

--- mean/modules/users/client/controllers/authentication.client.controller.js
+++ mean/modules/users/client/controllers/authentication.client.controller.js
@@ -14,7 +14,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
     if ($scope.authentication.user && !$scope.isDataEntry() ) {
       $location.path('/');
     }
-    
+     
     $scope.signup = function (isValid) {
       $scope.error = null;
 
